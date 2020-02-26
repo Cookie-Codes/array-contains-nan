6120867182a55414e2b8e342b69a826d90a4c95c
AngrySoilder <chaudhari041@outlook.com>
2020-02-26 20:27:41 +0530

    Added One More Test

--- src/test/initial.test.ts
+++ src/test/initial.test.ts
@@ -4,4 +4,5 @@ test('Testing Array Contains Nan', () => {
     expect(arrayContainsNaN(['a', 'b', true, []])).toBeFalsy();
     expect(arrayContainsNaN(['a', 'b', undefined, []])).toBeFalsy();
     expect(arrayContainsNaN(['a', 'b', NaN, []])).toBeTruthy();
+    expect(arrayContainsNaN([false, '', [], {}])).toBeFalsy();
 });
